bfdff8a4943d24d1bcb2da3f6473d8b32f2f5958
joseph yang <jyang2@students.ntc.edu>
2017-03-02 17:10:43 -0600

    Add project files.

---
+++ OrderEntrySystem/ViewModels/Framework/CommandViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OrderEntrySystem
+{
+    /// <summary>
+    /// The class representing a view model for commands.
+    /// </summary>
+    public class CommandViewModel : ViewModel
+    {
+
+        public string GroupName { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="displayName">The name of the command.</param>
+        /// <param name="command">The logic of the command</param>
+        public CommandViewModel(string displayName, ICommand command)
+            : this(displayName, command, false, false, null)
+        {
+        }
+
+        public CommandViewModel(string displayName, ICommand command, bool isDefault, bool isCancel, string groupName)
+            : base(displayName)
+        {
+            if (command == null)
+            {
+                throw new Exception("Command was null.");
+            }
+
+            this.Command = command;
+            this.IsCancel = isCancel;
+            this.IsDefault = isDefault;
+            this.GroupName = groupName;
+        }
+
+        /// <summary>
+        /// Gets the command to be viewed.
+        /// </summary>
+        public ICommand Command { get; private set; }
+
+        public bool IsDefault { get; private set; }
+
+        public bool IsCancel { get; private set; }
+    }
+}
